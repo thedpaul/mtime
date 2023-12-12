8175a7201001db6e22aa48db4354e59f984b2bca
thedpaul <dpaul2143@gmail.com>
2023-12-12 12:57:33 +0100

    better mtime.c - better formatting - library implementation shall only abort(3), no exit(3) allowed - usage of sigaction(3) to ignore SIGINT in parent process during   execution of child process instead of having a separate SIGINT   handler - better implementation of profile_cmd(), return depends on exit   status of the child process - mtime tool shall display the total cpu usage as a percentage   with 9 decimal places

--- mtime.c
+++ mtime.c
@@ -12,18 +12,24 @@
 #include "mtime.h"
 
 extern char **environ;
-static pid_t parent_pid;
 
-#define fatal(...)                                                             \
-  do {                                                                         \
-    fprintf(stderr, __VA_ARGS__);                                              \
-    exit(EXIT_FAILURE);                                                        \
+#define fatal(...) \
+  do { \
+    fprintf(stderr, __VA_ARGS__); \
+    exit(EXIT_FAILURE); \
   } while (0)
 
-static inline void die(const char *msg) {
+#ifdef MTIME_TOOL
+static inline void die(const char msg[]) {
   perror(msg);
   exit(EXIT_FAILURE);
 }
+#else
+static inline void die(const char msg[]) {
+  perror(msg);
+  abort();
+}
+#endif
 
 static inline long get_time_monotonic_ns(void) {
   struct timespec t;
@@ -32,52 +38,41 @@ static inline long get_time_monotonic_ns(void) {
   return t.tv_nsec + t.tv_sec * 1e9;
 }
 
-static void sigint_handler(int signum) {
-  if (getpid() == parent_pid) {
-    if (putc('\n', stdout) == EOF)
-      die("putc failed");
-    return;
-  }
-  if (kill(getpid(), SIGINT))
-    die("kill failed");
-}
-
 int mtime_init(char *envp[]) {
   environ = envp;
-  parent_pid = getpid();
-
-  struct sigaction sa = { 
-    .sa_handler = sigint_handler,
-    .sa_flags = SA_RESTART };
-
-  if (sigaction(SIGINT, &sa, NULL))
-    return -1;
-
   return 0;
 }
 
 int profile_cmd(char **cmd, long *runtime, struct rusage *r_usage) {
+  struct sigaction oact;
+  if (sigaction(SIGINT, &(struct sigaction){ .sa_handler = SIG_IGN }, &oact))
+    return -1;
   pid_t child_pid = fork();
   *runtime = get_time_monotonic_ns();
   if (child_pid == 0) {
-    if (execvp(*cmd, cmd))
-      return -1;
-    exit(EXIT_SUCCESS);
+    if (sigaction(SIGINT, &(struct sigaction){ .sa_handler = SIG_DFL }, NULL))
+      exit(EXIT_FAILURE);
+    execvp(*cmd, cmd);
+    exit(EXIT_FAILURE);
   }
-  if (child_pid == -1)
+  if (child_pid == -1)  {
+    if (sigaction(SIGINT, &oact, NULL))
+      die("sigaction failed");
     return -1;
+  }
   
   int status;
   if (waitpid(child_pid, &status, 0) == -1)
     return -1;
   *runtime = get_time_monotonic_ns() - *runtime;
-  if (WEXITSTATUS(status))
-    exit(EXIT_FAILURE);
+  if (sigaction(SIGINT, &oact, NULL))
+    die("sigaction failed");
+  status = WEXITSTATUS(status);
 
   if (getrusage(RUSAGE_CHILDREN, r_usage))
-    return -1;
+    return status;
 
-  return 0; 
+  return status; 
 }
 
 #ifdef MTIME_TOOL
@@ -90,22 +85,26 @@ int main(int argc, char *argv[], char *envp[]) {
 
   struct rusage r_usage;
   long runtime;
-  if (profile_cmd(argv + 1, &runtime, &r_usage))
+  if (profile_cmd(argv + 1, &runtime, &r_usage) == -1)
     die("profile_cmd failed");
   
   double user_cpu_time = r_usage.ru_utime.tv_sec + r_usage.ru_utime.tv_usec * 1e-6;
   double system_cpu_time =
       r_usage.ru_stime.tv_sec + r_usage.ru_stime.tv_usec * 1e-6;
   double total_time = runtime * 1e-9;
- 
+
+  if (putchar('\n') == EOF)
+    die("putchar failed");
+
+
   while (*(++argv)) {
     if (printf("%s ", *argv) < 0)
       die("printf failed");
   }
 
-  if (printf(" %.9fs user %.9fs system %d%% cpu %.9fs total\n",
+  if (printf(" %.9fs user %.9fs system %.9f%% cpu %.9fs total\n",
              user_cpu_time, system_cpu_time,
-             (int)(100 * (user_cpu_time + system_cpu_time) /
+             (100.0 * (user_cpu_time + system_cpu_time) /
                    total_time),
              total_time) < 0)
     die("printf failed");
